5697080cdf538fb6a18ea568fcd704987dab128a
LupusVII <jln.thirion@gmail.com>
2023-03-25 17:43:02 +0100

    Add translations

--- src/Resources/contao/dca/tl_pfs_discord_event.php
+++ src/Resources/contao/dca/tl_pfs_discord_event.php
@@ -63,7 +63,7 @@ $GLOBALS['TL_DCA']['tl_pfs_discord_event'] = [
     // Palettes
     'palettes' => [
         'default' => '
-            {global_legend},user,twitch_event,discord_event
+            {global_legend},user,twitch_event,discord_event,discord_event_name,discord_event_scheduled_start_time,discord_event_scheduled_end_time,discord_event_entity_metadata_location,discord_event_category_name
         ',
     ],
 
@@ -89,33 +89,33 @@ $GLOBALS['TL_DCA']['tl_pfs_discord_event'] = [
             'relation' => ['type' => 'belongsTo', 'load' => 'lazy'],
         ],
         'discord_event' => [
-            'search' => true,
             'inputType' => 'text',
+            'search' => true,
             'sql' => 'text NULL',
         ],
         'discord_event_name' => [
-            'search' => true,
             'inputType' => 'text',
+            'search' => true,
             'sql' => 'text NULL',
         ],
         'discord_event_scheduled_start_time' => [
-            'search' => true,
             'inputType' => 'text',
+            'search' => true,
             'sql' => 'text NULL',
         ],
         'discord_event_scheduled_end_time' => [
-            'search' => true,
             'inputType' => 'text',
+            'search' => true,
             'sql' => 'text NULL',
         ],
         'discord_event_entity_metadata_location' => [
-            'search' => true,
             'inputType' => 'text',
+            'search' => true,
             'sql' => 'text NULL',
         ],
         'discord_event_category_name' => [
-            'search' => true,
             'inputType' => 'text',
+            'search' => true,
             'sql' => 'text NULL',
         ],
     ],
