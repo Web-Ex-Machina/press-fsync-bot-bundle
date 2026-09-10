8c7589a54e16d12541a7109de67562aa8f3440e4
LupusVII <jln.thirion@ik.me>
2026-09-10 21:51:17 +0200

    Fix DisplayRuvonShamelistController

--- src/Controller/Frontend/DisplayRuvonShamelistController.php
+++ src/Controller/Frontend/DisplayRuvonShamelistController.php
@@ -7,6 +7,7 @@ namespace WEM\PressFsyncBotBundle\Controller\Frontend;
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
 use Contao\CoreBundle\Twig\FragmentTemplate;
+use Contao\Input;
 use Contao\ModuleModel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -28,6 +29,28 @@ class DisplayRuvonShamelistController extends AbstractFrontendModuleController
      */
     protected function getResponse(FragmentTemplate $template, ModuleModel $model, Request $request): Response
     {
+        if (Input::post('steamid')) {
+            $nbGamesPlayer = $this->getNumberOfGames(Input::post('steamid'));
+            $nbGamesRuvon = $this->getNumberOfGames('76561198084359321');
+
+            $template->steamid = Input::post('steamid');
+            $template->score = $nbGamesPlayer;
+            $template->scoreRuvon = $nbGamesRuvon;
+            $template->diff = $nbGamesPlayer / $nbGamesRuvon * 100;
+        }
+
         return $template->getResponse();
+    }
+
+    protected function getNumberOfGames(string $steamid): int 
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://api.steampowered.com/IWishlistService/GetWishlistItemCount/v1/?steamid=' . $steamid);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $r = curl_exec($ch);
+        $r = json_decode($r);
+        curl_close($ch);
+
+        return $r->response->count;
     }
 }
